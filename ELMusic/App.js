9020579c4fa043d7ebc1a8057c28180206c4f6a2
lariesther <larissafantinatti24@gmail.com>
2025-08-28 12:25:32 -0300

    sla

--- ELMusic/App.js
+++ ELMusic/App.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import Slider from '@react-native-community/slider';
 
 const {width, height}= Dimensions.get('window');
 export default function App() {
   return (
-    <SafeAreaView style={style.container}>
+    <SafeAreaView style={styles.container}>
       <View style ={ styles.main}>
 
+        <View style={[styles.imageWrapper, styles.elevation]}>
+          <Image 
+          source={require('./assets/img/gallo.png')}
+          style={styles.musicImage}
+          />
+        </View>
+
+        <View>
+          <Slider />
+        </View>
+
       </View>
-      <View style={style.footer}>
+      <View style={styles.footer}>
         <View style= {styles.iconWrapper}>
           <TouchableOpacity>
-            <IOnicons name='heart-outline' size={30} coplor="#88888"/>
+            <Ionicons name='heart-outline' size={30} coplor="#88888"/>
             </TouchableOpacity>
             <TouchableOpacity>
-            <IOnicons name='heart-outline' size={30} coplor="#88888"/>
+            <Ionicons name='heart-outline' size={30} coplor="#88888"/>
             </TouchableOpacity>
             <TouchableOpacity>
-            <IOnicons name='heart-outline' size={30} coplor="#88888"/>
+            <Ionicons name='heart-outline' size={30} coplor="#88888"/>
             </TouchableOpacity>
             <TouchableOpacity>
-            <IOnicons name='heart-outline' size={30} coplor="#88888"/>
+            <Ionicons name='heart-outline' size={30} coplor="#88888"/>
             </TouchableOpacity>
         </View>
         </View>
@@ -35,7 +47,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#222831',
+  },
+  main: {
+    flex: 1, 
     alignItems: 'center',
     justifyContent: 'center',
   },
+  footer: {
+    width: width,
+    alignItems: 'center',
+    paddingVertical: 25,
+    borderTopColor: "#393E45",
+    borderTopWidth: 1,
+  }, 
+  iconWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%'
+  },
+  imageWrapper: {
+    widht: 340,
+    height: 360,
+    marginVertical: 20,
+  },
+  elevation: {
+    elevation: 5,
+    shadowOffset: {
+      width: 5,
+      height: 5
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84
+  },
+  musicImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 15
+  },
+
 });
