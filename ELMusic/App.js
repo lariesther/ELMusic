fc9f28e6a115fef743d90ae034cd2a3cfc9226e2
lariesther <larissafantinatti24@gmail.com>
2025-09-01 11:40:12 -0300

    finalizada a interface grafica

--- ELMusic/App.js
+++ ELMusic/App.js
@@ -4,42 +4,76 @@ import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Ima
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Slider from '@react-native-community/slider';
 
-const {width, height}= Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style ={ styles.main}>
+      <View style={styles.main}>
 
         <View style={[styles.imageWrapper, styles.elevation]}>
-          <Image 
-          source={require('./assets/img/gallo.png')}
-          style={styles.musicImage}
+          <Image
+            source={require('./assets/img/gallo.png')}
+            style={styles.musicImage}
           />
         </View>
 
         <View>
-          <Slider />
+          <Text style={[styles.songContent, styles.songTitle]}>
+            Título da Música
+          </Text>
+          <Text style={[styles.songContent, styles.songArtitst]}>
+            Autor da Música
+          </Text>
+        </View>
+
+        <View>
+          <Slider
+            stye={styles.progressBar}
+            value={10}
+            minimumValue={0}
+            maximumValue={100}
+            thumbTintColor='#FFD369'
+            minimumTrackTintColor='#FFD369'
+            maximumTrackTintColor='#fff'
+            onSlidingComplete={() => { }} />
+
+          <View style={styles.progressLevelDuration}>
+            <Text style={styles.progressLabelText}>00:00</Text>
+            <Text style={styles.progressLabelText}>00:00</Text>
+          </View>
+        </View>
+
+        <View style={styles.musicControlsContainer}>
+          <TouchableOpacity>
+            <Ionicons name='play-skip-back-outline' size={35} color='#FFD369' />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name='pause-circle' size={75} color='#FFD369'></Ionicons>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name='play-skip-forward-outline' size={35} color='#FFD369'></Ionicons>
+          </TouchableOpacity>
         </View>
 
       </View>
       <View style={styles.footer}>
-        <View style= {styles.iconWrapper}>
+        <View style={styles.iconWrapper}>
           <TouchableOpacity>
-            <Ionicons name='heart-outline' size={30} coplor="#88888"/>
-            </TouchableOpacity>
-            <TouchableOpacity>
-            <Ionicons name='heart-outline' size={30} coplor="#88888"/>
-            </TouchableOpacity>
-            <TouchableOpacity>
-            <Ionicons name='heart-outline' size={30} coplor="#88888"/>
-            </TouchableOpacity>
-            <TouchableOpacity>
-            <Ionicons name='heart-outline' size={30} coplor="#88888"/>
-            </TouchableOpacity>
+            <Ionicons name='heart-outline' size={30} coplor="#88888" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name='heart-outline' size={30} coplor="#88888" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name='heart-outline' size={30} coplor="#88888" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Ionicons name='heart-outline' size={30} coplor="#88888" />
+          </TouchableOpacity>
         </View>
-        </View>
+      </View>
       <StatusBar style="light" />
-      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
@@ -49,7 +83,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#222831',
   },
   main: {
-    flex: 1, 
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -59,7 +93,7 @@ const styles = StyleSheet.create({
     paddingVertical: 25,
     borderTopColor: "#393E45",
     borderTopWidth: 1,
-  }, 
+  },
   iconWrapper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -83,6 +117,40 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     borderRadius: 15
+  },
+  songContent: {
+    textAlign: 'center',
+    color: '#EEEEEE'
+  },
+  songTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+
+  },
+  songArtitst: {
+    fontSize: 16,
+    fontWeight: '300'
+  },
+  progressBar: {
+    width: 350,
+    height: 40,
+    marginTop: 20,
+  },
+  progressLevelDuration: {
+    width: 340,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  progressLabelText: {
+    color: '#fff',
+    fontWeight: '500',
+  },
+  musicControlsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '60%',
+    marginTop: 10,
   },
 
 });
